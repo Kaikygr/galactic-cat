95b7504c6449ed1e304b4e51f5a125721c29040d
kaikygr <kaikygomesriberoof@gmail.com>
2025-03-02 15:31:26 -0400

    remove comentários desnecessários e melhora a legibilidade do código em sticker.js

--- src/modules/sticker/sticker.js
+++ src/modules/sticker/sticker.js
@@ -7,7 +7,6 @@ const execProm = util.promisify(exec);
 
 const { getFileBuffer } = require("../../utils/functions");
 
-// Criação do diretório temporário, se não existir
 const tempDir = path.join(__dirname, "..", "..", "temp");
 if (!fs.existsSync(tempDir)) {
   fs.mkdirSync(tempDir, { recursive: true });
@@ -15,18 +14,14 @@ if (!fs.existsSync(tempDir)) {
 
 async function processSticker(client, info, sender, from, text, isMedia, isQuotedVideo, isQuotedImage, config, getFileBuffer) {
   try {
-    // Define o filtro padrão para vídeo e imagem
     let filtro = "fps=10,scale=512:512";
 
-    // Função para envio de mensagens rápidas
     const userMessageReport = async msg => {
       await client.sendMessage(from, { text: msg }, { quoted: info });
     };
 
     let encmedia, mediaBuffer, mediaExtension;
-    // Processamento da mídia, verifica se é vídeo ou imagem e aplica limitações de duração 
     if ((isMedia && info.message.videoMessage) || isQuotedVideo) {
-      // Obtém a duração do vídeo
       const videoDuration = isMedia && info.message.videoMessage ? info.message.videoMessage.seconds : info.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds;
       if (videoDuration >= (isQuotedVideo ? 35 : 11)) {
         return userMessageReport("Vídeo muito longo para sticker animada.");
@@ -35,7 +30,6 @@ async function processSticker(client, info, sender, from, text, isMedia, isQuote
       mediaBuffer = await getFileBuffer(encmedia, "video");
       mediaExtension = ".mp4";
     } else if ((isMedia && !info.message.videoMessage) || isQuotedImage) {
-      // Trata a imagem
       encmedia = isQuotedImage ? info.message.extendedTextMessage.contextInfo.quotedMessage.imageMessage : info.message.imageMessage;
       mediaBuffer = await getFileBuffer(encmedia, "image");
       mediaExtension = ".jpg";
@@ -43,20 +37,16 @@ async function processSticker(client, info, sender, from, text, isMedia, isQuote
       return userMessageReport("Envie ou cite uma imagem ou vídeo para criar o sticker.");
     }
 
-    // Altera o filtro para imagens, se necessário
     if (mediaExtension === ".jpg") {
       filtro = "scale=512:512";
     }
 
-    // Salva a mídia em um arquivo temporário
     const mediaPath = path.join(tempDir, `temp_${Date.now()}${mediaExtension}`);
     fs.writeFileSync(mediaPath, mediaBuffer);
 
-    // Cria o sticker em formato webp usando ffmpeg
     const outputPath = path.join(tempDir, `sticker_${Date.now()}.webp`);
     await execProm(`ffmpeg -i "${mediaPath}" -vcodec libwebp -lossless 1 -loop 0 -preset default -an -vf "${filtro}" "${outputPath}"`);
 
-    // Prepara os metadados para o sticker
     const json = {
       "sticker-pack-name": `User: ${info.pushName || sender}`,
       "sticker-pack-publisher": `Owner: ${config.owner.name}`
@@ -68,7 +58,6 @@ async function processSticker(client, info, sender, from, text, isMedia, isQuote
     const metaPath = path.join(tempDir, `meta_${Date.now()}.temp.exif`);
     fs.writeFileSync(metaPath, exifBuffer);
 
-    // Verifica a presença do webpmux e auxilia na aplicação dos metadados
     let webpmuxPath = "";
     try {
       webpmuxPath = (await execProm("which webpmux")).stdout.trim();
@@ -79,11 +68,9 @@ async function processSticker(client, info, sender, from, text, isMedia, isQuote
     await execProm(`"${webpmuxPath}" -set exif "${metaPath}" "${outputPath}" -o "${outputPath}"`);
     fs.unlinkSync(metaPath);
 
-    // Envia o sticker para o usuário e limpa os arquivos temporários
     await client.sendMessage(from, { sticker: fs.readFileSync(outputPath) }, { quoted: info });
     fs.unlinkSync(mediaPath);
   } catch (error) {
-    // Trata erros e informa o usuário
     await client.sendMessage(from, { text: "Error durante o processamento." }, { quoted: info });
     console.log(error);
   }
