22063428dbaaccc2759bc33127f47e484f3c4326
kaikygr <kaikygomesriberoof@gmail.com>
2025-03-02 21:08:48 -0400

    substitui a configuração do Winston por um módulo de logger personalizado em botController.js

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -9,20 +9,7 @@ const ConfigfilePath = path.join(__dirname, "../config/options.json");
 const config = require(ConfigfilePath);
 const messageController = require(path.join(__dirname, "./consoleMessage"));
 
-const winston = require("winston");
-const logger = winston.createLogger({
-  level: "info",
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple())
-    }),
-    new winston.transports.File({
-      filename: "./logs/error.log",
-      level: "error",
-      format: winston.format.combine(winston.format.timestamp(), winston.format.json())
-    })
-  ]
-});
+const logger = require("../utils/logger")
 
 const maxAttempts = 3;
 const delayMs = 1000;
