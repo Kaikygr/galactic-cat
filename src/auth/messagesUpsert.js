952de2aa417f5f3de0f84cab3ec6eff6567a7a51
kaikygr <kaikygomesribeiroof@gmail.com>
2025-03-16 17:37:38 -0400

    Adicionar registro de informações e identificar remetente em mensagens

--- src/auth/messagesUpsert.js
+++ src/auth/messagesUpsert.js
@@ -1,3 +1,10 @@
+const { from } = require("multistream");
+const logger = require("../utils/logger");
+const baileys = require("@whiskeysockets/baileys");
+
 module.exports = async (data, client) => {
-  console.log("Data e cliente foram recebidos com sucesso");
+  logger.info("Data e cliente foram recebidos com sucesso");
+  const from = data.messages[0].key.remoteJid;
+  const isGroup = from.endsWith("@g.us");
+  const sender = isGroup ? data.messages[0].key.participant : data.messages[0].key.remoteJid;
 };
