e65b5fd3918341f017767155e8393e3e77b94ae2
kaikygr <kaikygomesribeiroof@gmail.com>
2025-04-01 22:25:43 -0400

    refatorar função handleWhatsAppUpdate para melhorar legibilidade e organização do código

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -13,97 +13,80 @@ const { getFileBuffer } = require(path.join(__dirname, "../utils/functions"));
 const { preProcessMessage, processPrefix, getQuotedChecks, getExpiration } = require(path.join(__dirname, "./messageTypeController"));
 
 async function handleWhatsAppUpdate(upsert, client) {
-  for (const info of upsert?.messages || []) {
-    if (!info || !info.key || !info.message) return;
-    if (info.key.fromMe) return;
+   for (const info of upsert?.messages || []) {
+      if (!info || !info.key || !info.message) return;
+      if (info.key.fromMe) return;
 
-    const from = info.key.remoteJid;
-    const isGroup = from.endsWith("@g.us");
-    const sender = isGroup ? info.key.participant : info.key.remoteJid;
-    const userName = info?.pushName || "Desconhecido";
-    const expirationMessage = getExpiration(info);
+      const from = info.key.remoteJid;
+      const isGroup = from.endsWith("@g.us");
+      const sender = isGroup ? info.key.participant : info.key.remoteJid;
+      const userName = info?.pushName || "Desconhecido";
+      const expirationMessage = getExpiration(info);
 
-    const { type, body, isMedia } = preProcessMessage(info);
-    const prefixResult = processPrefix(body, config.bot.globalSettings.prefix);
-    if (!prefixResult) return;
+      const { type, body, isMedia } = preProcessMessage(info);
+      const prefixResult = processPrefix(body, config.bot.globalSettings.prefix);
+      if (!prefixResult) return;
 
-    const { comando, args } = prefixResult;
-    const text = args.join(" ");
-    const content = JSON.stringify(info.message);
+      const { comando, args } = prefixResult;
+      const text = args.join(" ");
+      const content = JSON.stringify(info.message);
 
-    const isOwner = sender === config.owner.number;
-    const ownerPhoneNumber = config.owner.number;
-    const ownerName = config.owner.name;
+      const isOwner = sender === config.owner.number;
+      const ownerPhoneNumber = config.owner.number;
+      const ownerName = config.owner.name;
 
-    const { isQuotedMsg, isQuotedImage, isQuotedVideo, isQuotedDocument, isQuotedAudio, isQuotedSticker, isQuotedContact, isQuotedLocation, isQuotedProduct } = getQuotedChecks(type, content);
+      const { isQuotedMsg, isQuotedImage, isQuotedVideo, isQuotedDocument, isQuotedAudio, isQuotedSticker, isQuotedContact, isQuotedLocation, isQuotedProduct } = getQuotedChecks(type, content);
 
-    function getGroupAdmins(participants) {
-      const admins = [];
-      for (const participant of participants) {
-        if (participant.admin === "admin" || participant.admin === "superadmin") {
-          admins.push(participant.id);
-        }
+      function getGroupAdmins(participants) {
+         const admins = [];
+         for (const participant of participants) {
+            if (participant.admin === "admin" || participant.admin === "superadmin") {
+               admins.push(participant.id);
+            }
+         }
+         return admins;
       }
-      return admins;
-    }
-    const groupMeta = isGroup ? await client.groupMetadata(from) : null;
-    const groupFormattedData = groupMeta ? JSON.stringify(groupMeta, null, 2) : null;
-    const isGroupAdmin = isGroup ? getGroupAdmins(groupMeta.participants).includes(sender) : false;
+      const groupMeta = isGroup ? await client.groupMetadata(from) : null;
+      const groupFormattedData = groupMeta ? JSON.stringify(groupMeta, null, 2) : null;
+      const isGroupAdmin = isGroup ? getGroupAdmins(groupMeta.participants).includes(sender) : false;
 
-    const isQuotedUser = Object.entries(info.message || {}).reduce((acc, [_, value]) => {
-      if (value?.contextInfo) {
-        const mencionados = value.contextInfo.mentionedJid || [];
-        const participante = value.contextInfo.participant ? [value.contextInfo.participant] : [];
-        return [...acc, ...mencionados, ...participante];
-      }
-      return acc;
-    }, []);
+      const isQuotedUser = Object.entries(info.message || {}).reduce((acc, [_, value]) => {
+         if (value?.contextInfo) {
+            const mencionados = value.contextInfo.mentionedJid || [];
+            const participante = value.contextInfo.participant ? [value.contextInfo.participant] : [];
+            return [...acc, ...mencionados, ...participante];
+         }
+         return acc;
+      }, []);
 
-    switch (comando) {
-      case "cat":
-      case "gemini": {
-        await processAIContent(client, from, info, expirationMessage, sender, userName, text);
-        break;
-      }
+      switch (comando) {
+         case "cat":
+         case "gemini": {
+            await processAIContent(client, from, info, expirationMessage, sender, userName, text);
+            break;
+         }
 
-      case "sticker":
-      case "s": {
-        await processSticker(client, info, expirationMessage, sender, from, text, isMedia, isQuotedVideo, isQuotedImage, config, getFileBuffer);
-        break;
-      }
+         case "sticker":
+         case "s": {
+            await processSticker(client, info, expirationMessage, sender, from, text, isMedia, isQuotedVideo, isQuotedImage, config, getFileBuffer);
+            break;
+         }
 
-      case "grupo": {
-        try {
-          if (text === "--info") {
-            await processGroupMetrics(client, info, from, expirationMessage);
-          } else if (text.startsWith("--me")) {
-            const userId = sender;
-            await processUserMetrics(client, info, from, expirationMessage, userId);
-          } else {
-            await client.sendMessage(from, { text: "❌ Comando inválido. Use .grupo --me para obter informações." }, { quoted: info, ephemeralExpiration: expirationMessage });
-          }
-        } catch (error) {
-          enviar(from, "❌ Ocorreu um erro ao processar sua solicitação. Tente novamente mais tarde.");
-          console.error("Erro:", error);
-        }
-        break;
+         case "eval": {
+            if (!isOwner) {
+               await client.sendMessage(from, { text: "❌ O comando eval é restrito ao dono." }, { quoted: info, ephemeralExpiration: expirationMessage });
+               break;
+            }
+            try {
+               const result = eval(text);
+               await client.sendMessage(from, { text: `Resultado: ${result}` }, { quoted: info, ephemeralExpiration: expirationMessage });
+            } catch (error) {
+               await client.sendMessage(from, { text: `Erro ao executar o comando: ${error.message}` }, { quoted: info, ephemeralExpiration: expirationMessage });
+            }
+            break;
+         }
       }
-
-      case "eval": {
-        if (!isOwner) {
-          await client.sendMessage(from, { text: "❌ O comando eval é restrito ao dono." }, { quoted: info, ephemeralExpiration: expirationMessage });
-          break;
-        }
-        try {
-          const result = eval(text);
-          await client.sendMessage(from, { text: `Resultado: ${result}` }, { quoted: info, ephemeralExpiration: expirationMessage });
-        } catch (error) {
-          await client.sendMessage(from, { text: `Erro ao executar o comando: ${error.message}` }, { quoted: info, ephemeralExpiration: expirationMessage });
-        }
-        break;
-      }
-    }
-  }
+   }
 }
 
 module.exports = handleWhatsAppUpdate;
