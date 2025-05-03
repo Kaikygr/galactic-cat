8824dbd5c66e8c563056ddabe9377f651165b695
kaikygr <kaikygomesribeiroof@gmail.com>
2025-05-02 20:37:45 -0400

    fix: corrigindo modo de inicialização

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 module.exports = {
   apps: [
     {
-      name: process.env.ECOSYTEM_NAME,
+      name: process.env.ECOSYSTEM_NAME || "bot-system",
       script: "./src/auth/connection.js",
       exec_mode: "fork",
       instances: 1,
       watch: false,
-      max_memory_restart: "1000M",
+      max_memory_restart: "2000M",
+      merge_logs: true,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
   ],
 };
