3dffb0d31c781ae6d0d4649ac59b6c02f3f6f32d
kaikygr <kaikygomesribeiroof@gmail.com>
2025-05-05 21:56:32 -0400

    chore: ajustar formatação e valores de memória no ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   apps: [
     {
-      name: process.env.ECOSYSTEM_NAME || "bot-system",
-      script: "./src/auth/connection.js",
-      exec_mode: "fork",
+      name: process.env.ECOSYSTEM_NAME || 'bot-system',
+      script: './src/auth/connection.js',
+      exec_mode: 'fork',
       instances: 1,
       watch: false,
-      max_memory_restart: "2000M",
+      max_memory_restart: '500M',
       merge_logs: true,
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: 'development',
       },
       env_production: {
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
       },
     },
   ],
